21ce4758c6409bb4a8e8d523cedd225d32350878
shristi58 <kumarishristi22004@gmail.com>
2025-11-03 20:00:37 +0530

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = "mySecretKey";
+
+export function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Token required" });
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+    req.user = user;
+    next();
+  });
+}
+
+export function authorizeRoles(...allowedRoles) {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Access denied: insufficient role" });
+    }
+    next();
+  };
+}
